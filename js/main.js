84570ddf4fb6c225310bba25cd79e2e36562e099
jwmcgrory <jwmcgrory@gmail.com>
2018-03-14 16:54:46 -0600

    jumpstart project

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+  $('.carousel').slick({
+   pauseOnHover: false,
+   pauseOnFocus: false,
+   arrows: false,
+   fade: false,
+   autoplay: true,
+   draggable: false,
+ });
+});
